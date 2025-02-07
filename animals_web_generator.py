679a8aa3e7c613aff6e12438f41d91112bb6528b
Lee Roy Stevenson <leroysheep@gmail.com>
2025-02-07 03:26:47 +0100

    bring animal data to html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -10,15 +10,55 @@ def load_data(data_path):
         return json.load(handle)
 
 
-animals_data = load_data('animals_data.json')
+def create_data_string(animal_data):
+    """
+    function to create a string with selected data from the json file
+    :param animal_data:
+    :return:
+    """
+    output = ''
+    for animal in animal_data:
+        if "name" in animal:
+            output += f'Name: {animal["name"]}\n'
+        if "diet" in animal["characteristics"]:
+            output += f'Diet: {animal["characteristics"]["diet"]}\n'
+        if "location" in animal:
+            output += f'Location: {animal["location"][0]}\n'
+        if "type" in animal["characteristics"]:
+            output += f'Type: {animal["characteristics"]["type"]}\n'
+        output += '\n'
+    return output
 
-for animal in animals_data:
-    if "name" in animal:
-        print(f'Name: {animal["name"]}')
-    if "diet" in animal["characteristics"]:
-        print(f'Diet: {animal["characteristics"]["diet"]}')
-    if "location" in animal:
-        print(f'Location: {animal["location"][0]}')
-    if "type" in animal["characteristics"]:
-        print(f'Type: {animal["characteristics"]["type"]}')
-    print()
+
+def html_reader(html_file_path):
+    """
+    function to read from html file and return input as string
+    :param html_file_path:
+    :return: html_script_string
+    """
+    with open(html_file_path, 'r') as handler:
+        return handler.read()
+
+
+def html_writer(html_file_path, new_string):
+    """
+    function to save a html file at html_file_path with new_string as input
+    :param1 html_file_path:
+    :param2 new_string:
+    :return:
+    """
+    with open(html_file_path, 'w') as handler:
+        handler.write(new_string)
+
+
+def main():
+    """
+    main function combining all functionality of this file
+    :return:
+    """
+    text_input = create_data_string(load_data('animals_data.json'))
+    html_writer('animals_template.html',html_reader('animals_template.html').replace('__REPLACE_ANIMALS_INFO__',text_input))
+
+
+if __name__ == '__main__':
+    main()
